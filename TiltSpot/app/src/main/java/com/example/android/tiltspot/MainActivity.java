1f79e2b6dac736f0c2fcee2b4ed8d46c2155f5a5
Rafian666 <rafianw123@gmail.cmo>
2023-05-15 15:17:26 +0700

    Menambahhkan Save Button

--- TiltSpot/app/src/main/java/com/example/android/tiltspot/MainActivity.java
+++ TiltSpot/app/src/main/java/com/example/android/tiltspot/MainActivity.java
@@ -26,12 +26,22 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Display;
 import android.view.Surface;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.FileReader;
 
 public class MainActivity extends AppCompatActivity
         implements SensorEventListener {
+
+    Button save;
 
     // System sensor manager instance.
     private SensorManager mSensorManager;
@@ -60,6 +70,10 @@ public class MainActivity extends AppCompatActivity
     // System display. Need this for determining rotation.
     private Display mDisplay;
 
+    private TextView label;
+    private TextView label1;
+    private TextView label2;
+
     // Very small values for the accelerometer (on all three axes) should
     // be interpreted as 0. This value is the amount of acceptable
     // non-zero drift.
@@ -70,13 +84,17 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        label = (TextView) findViewById(R.id.label_azimuth);
         mTextSensorAzimuth = (TextView) findViewById(R.id.value_azimuth);
+        label1 = (TextView) findViewById(R.id.label_pitch);
         mTextSensorPitch = (TextView) findViewById(R.id.value_pitch);
+        label2 = (TextView) findViewById(R.id.label_roll);
         mTextSensorRoll = (TextView) findViewById(R.id.value_roll);
         mSpotTop = (ImageView) findViewById(R.id.spot_top);
         mSpotBottom = (ImageView) findViewById(R.id.spot_bottom);
         mSpotLeft = (ImageView) findViewById(R.id.spot_left);
         mSpotRight = (ImageView) findViewById(R.id.spot_right);
+        save = (Button) findViewById(R.id.save);
 
         // Get accelerometer and magnetometer sensors from the sensor manager.
         // The getDefaultSensor() method returns null if the sensor
@@ -91,6 +109,360 @@ public class MainActivity extends AppCompatActivity
         // Get the display from the window manager (for rotation).
         WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
         mDisplay = wm.getDefaultDisplay();
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!mTextSensorAzimuth.getText().toString().isEmpty()) {
+                    File file = new File(MainActivity.this.getFilesDir(), "text");
+                    if (!file.exists()) {
+                        file.mkdir();
+                    }
+                    try {
+                        File gpxfile = new File(file, "Tilt_Spot_Data");
+                        FileWriter writer = new FileWriter(gpxfile);
+                        writer.append(label.getText().toString());
+                        writer.append(mTextSensorAzimuth.getText().toString());
+                        writer.append(label1.getText().toString());
+                        writer.append(mTextSensorPitch.getText().toString());
+                        writer.append(label2.getText().toString());
+                        writer.append(mTextSensorRoll.getText().toString());
+                        writer.flush();
+                        writer.close();
+
+//                        mTextSensorAzimuth.setText(readFile());
+//
+//                        mTextSensorPitch.setText(readFile());
+//
+//                        mTextSensorRoll.setText(readFile());
+                        Toast.makeText(getBaseContext(), "File saved successfully!",
+                                Toast.LENGTH_SHORT).show();
+                    } catch (Exception e) {
+                    }
+                }
+            }
+        });
+    }
+
+    private String readFile() {
+        File fileEvents = new File(MainActivity.this.getFilesDir() + "/text/sample");
+        StringBuilder text = new StringBuilder();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fileEvents));
+            String line;
+            while ((line = br.readLine()) != null) {
+                text.append(line);
+                text.append(' ');
+            }
+            br.close();
+        } catch (IOException e) {
+        }
+        String result = text.toString();
+        return result;
+    }
+
+    /**
+     * Listeners for the sensors are registered in this callback so that
+     * they can be unregistered in onStop().
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Listeners for the sensors are registered in this callback and
+        // can be unregistered in onStop().
+        //
+        // Check to ensure sensors are available before registering listeners.
+        // Both listeners are registered with a "normal" amount of delay
+        // (SENSOR_DELAY_NORMAL).
+        if (mSensorAccelerometer != null) {
+            mSensorManager.registerListener(this, mSensorAccelerometer,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        if (mSensorMagnetometer != null) {
+            mSensorManager.registerListener(this, mSensorMagnetometer,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        // Unregister all sensor listeners in this callback so they don't
+        // continue to use resources when the app is stopped.
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        // The sensor type (as defined in the Sensor class).
+        int sensorType = sensorEvent.sensor.getType();
+
+        // The sensorEvent object is reused across calls to onSensorChanged().
+        // clone() gets a copy so the data doesn't change out from under us
+        switch (sensorType) {
+            case Sensor.TYPE_ACCELEROMETER:
+                mAccelerometerData = sensorEvent.values.clone();
+                break;
+            case Sensor.TYPE_MAGNETIC_FIELD:
+                mMagnetometerData = sensorEvent.values.clone();
+                break;
+            default:
+                return;
+        }
+        // Compute the rotation matrix: merges and translates the data
+        // from the accelerometer and magnetometer, in the device coordinate
+        // system, into a matrix in the world's coordinate system.
+        //
+        // The second argument is an inclination matrix, which isn't
+        // used in this example.
+        float[] rotationMatrix = new float[9];
+        boolean rotationOK = SensorManager.getRotationMatrix(rotationMatrix,
+                null, mAccelerometerData, mMagnetometerData);
+
+        // Remap the matrix based on current device/activity rotation.
+        float[] rotationMatrixAdjusted = new float[9];
+        switch (mDisplay.getRotation()) {
+            case Surface.ROTATION_0:
+                rotationMatrixAdjusted = rotationMatrix.clone();
+                break;
+            case Surface.ROTATION_90:
+                SensorManager.remapCoordinateSystem(rotationMatrix,
+                        SensorManager.AXIS_Y, SensorManager.AXIS_MINUS_X,
+                        rotationMatrixAdjusted);
+                break;
+            case Surface.ROTATION_180:
+                SensorManager.remapCoordinateSystem(rotationMatrix,
+                        SensorManager.AXIS_MINUS_X, SensorManager.AXIS_MINUS_Y,
+                        rotationMatrixAdjusted);
+                break;
+            case Surface.ROTATION_270:
+                SensorManager.remapCoordinateSystem(rotationMatrix,
+                        SensorManager.AXIS_MINUS_Y, SensorManager.AXIS_X,
+                        rotationMatrixAdjusted);
+                break;
+        }
+
+        // Get the orientation of the device (azimuth, pitch, roll) based
+        // on the rotation matrix. Output units are radians.
+        float orientationValues[] = new float[3];
+        if (rotationOK) {
+            SensorManager.getOrientation(rotationMatrixAdjusted,
+                    orientationValues);
+        }
+
+        // Pull out the individual values from the array.
+        float azimuth = orientationValues[0];
+        float pitch = orientationValues[1];
+        float roll = orientationValues[2];
+
+        // Pitch and roll values that are close to but not 0 cause the
+        // animation to flash a lot. Adjust pitch and roll to 0 for very
+        // small values (as defined by VALUE_DRIFT).
+        if (Math.abs(pitch) < VALUE_DRIFT) {
+            pitch = 0;
+        }
+        if (Math.abs(roll) < VALUE_DRIFT) {
+            roll = 0;
+        }
+
+        // Fill in the string placeholders and set the textview text.
+        mTextSensorAzimuth.setText(getResources().getString(
+                R.string.value_format, azimuth));
+        mTextSensorPitch.setText(getResources().getString(
+                R.string.value_format, pitch));
+        mTextSensorRoll.setText(getResources().getString(
+                R.string.value_format, roll));
+
+        // Reset all spot values to 0. Without this animation artifacts can
+        // happen with fast tilts.
+        mSpotTop.setAlpha(0f);
+        mSpotBottom.setAlpha(0f);
+        mSpotLeft.setAlpha(0f);
+        mSpotRight.setAlpha(0f);
+
+        // Set spot color (alpha/opacity) equal to pitch/roll.
+        // this is not a precise grade (pitch/roll can be greater than 1)
+        // but it's close enough for the animation effect.
+        if (pitch > 0) {
+            mSpotBottom.setAlpha(pitch);
+        } else {
+            mSpotTop.setAlpha(Math.abs(pitch));
+        }
+        if (roll > 0) {
+            mSpotLeft.setAlpha(roll);
+        } else {
+            mSpotRight.setAlpha(Math.abs(roll));
+        }
+    }
+
+    /**
+     * Must be implemented to satisfy the SensorEventListener interface;
+     * unused in this app.
+     */
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+    }
+}/*
+ * Copyright (C) 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.tiltspot;
+
+        import android.content.Context;
+        import android.content.pm.ActivityInfo;
+        import android.hardware.Sensor;
+        import android.hardware.SensorEvent;
+        import android.hardware.SensorEventListener;
+        import android.hardware.SensorManager;
+        import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.view.Display;
+        import android.view.Surface;
+        import android.view.View;
+        import android.view.WindowManager;
+        import android.widget.ImageView;
+        import android.widget.TextView;
+        import android.widget.Button;
+        import android.widget.Toast;
+        import java.io.File;
+        import java.io.FileWriter;
+        import java.io.BufferedReader;
+        import java.io.IOException;
+        import java.io.FileReader;
+
+public class MainActivity extends AppCompatActivity
+        implements SensorEventListener {
+
+    Button save;
+
+    // System sensor manager instance.
+    private SensorManager mSensorManager;
+
+    // Accelerometer and magnetometer sensors, as retrieved from the
+    // sensor manager.
+    private Sensor mSensorAccelerometer;
+    private Sensor mSensorMagnetometer;
+
+    // Current data from accelerometer & magnetometer.  The arrays hold values
+    // for X, Y, and Z.
+    private float[] mAccelerometerData = new float[3];
+    private float[] mMagnetometerData = new float[3];
+
+    // TextViews to display current sensor values.
+    private TextView mTextSensorAzimuth;
+    private TextView mTextSensorPitch;
+    private TextView mTextSensorRoll;
+
+    // ImageView drawables to display spots.
+    private ImageView mSpotTop;
+    private ImageView mSpotBottom;
+    private ImageView mSpotLeft;
+    private ImageView mSpotRight;
+
+    // System display. Need this for determining rotation.
+    private Display mDisplay;
+
+    private TextView label;
+    private TextView label1;
+    private TextView label2;
+
+    // Very small values for the accelerometer (on all three axes) should
+    // be interpreted as 0. This value is the amount of acceptable
+    // non-zero drift.
+    private static final float VALUE_DRIFT = 0.05f;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        label = (TextView) findViewById(R.id.label_azimuth);
+        mTextSensorAzimuth = (TextView) findViewById(R.id.value_azimuth);
+        label1 = (TextView) findViewById(R.id.label_pitch);
+        mTextSensorPitch = (TextView) findViewById(R.id.value_pitch);
+        label2 = (TextView) findViewById(R.id.label_roll);
+        mTextSensorRoll = (TextView) findViewById(R.id.value_roll);
+        mSpotTop = (ImageView) findViewById(R.id.spot_top);
+        mSpotBottom = (ImageView) findViewById(R.id.spot_bottom);
+        mSpotLeft = (ImageView) findViewById(R.id.spot_left);
+        mSpotRight = (ImageView) findViewById(R.id.spot_right);
+        save = (Button) findViewById(R.id.save);
+
+        // Get accelerometer and magnetometer sensors from the sensor manager.
+        // The getDefaultSensor() method returns null if the sensor
+        // is not available on the device.
+        mSensorManager = (SensorManager) getSystemService(
+                Context.SENSOR_SERVICE);
+        mSensorAccelerometer = mSensorManager.getDefaultSensor(
+                Sensor.TYPE_ACCELEROMETER);
+        mSensorMagnetometer = mSensorManager.getDefaultSensor(
+                Sensor.TYPE_MAGNETIC_FIELD);
+
+        // Get the display from the window manager (for rotation).
+        WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
+        mDisplay = wm.getDefaultDisplay();
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!mTextSensorAzimuth.getText().toString().isEmpty()) {
+                    File file = new File(MainActivity.this.getFilesDir(), "text");
+                    if (!file.exists()) {
+                        file.mkdir();
+                    }
+                    try {
+                        File gpxfile = new File(file, "Tilt_Spot_Data");
+                        FileWriter writer = new FileWriter(gpxfile);
+                        writer.append(label.getText().toString());
+                        writer.append(mTextSensorAzimuth.getText().toString());
+                        writer.append(label1.getText().toString());
+                        writer.append(mTextSensorPitch.getText().toString());
+                        writer.append(label2.getText().toString());
+                        writer.append(mTextSensorRoll.getText().toString());
+                        writer.flush();
+                        writer.close();
+
+//                        mTextSensorAzimuth.setText(readFile());
+//
+//                        mTextSensorPitch.setText(readFile());
+//
+//                        mTextSensorRoll.setText(readFile());
+                        Toast.makeText(getBaseContext(), "File saved successfully!",
+                                Toast.LENGTH_SHORT).show();
+                    } catch (Exception e) {
+                    }
+                }
+            }
+        });
+    }
+
+    private String readFile() {
+        File fileEvents = new File(MainActivity.this.getFilesDir() + "/text/sample");
+        StringBuilder text = new StringBuilder();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fileEvents));
+            String line;
+            while ((line = br.readLine()) != null) {
+                text.append(line);
+                text.append(' ');
+            }
+            br.close();
+        } catch (IOException e) {
+        }
+        String result = text.toString();
+        return result;
     }
 
     /**
